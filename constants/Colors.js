c9b34ac39f976e7e1fd087d71cb06bb8b9970f19
alikareem123 <55332168+alikareem123@users.noreply.github.com>
2024-04-05 16:20:27 +0530

    The portrait view of the app is done

---
+++ constants/Colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+    primary500: "#72063c",
+    primary600: "#8e0505fa1",
+    primary700: "#58022d",
+    primary800: "#3b021f",
+    accent500: "#ddb52f",
+
+};
+export default Colors;
